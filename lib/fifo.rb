40a22513c35d1d79c9faba74967bf0ce790605c5
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-05 23:42:00 +0000

    proper loading paths.

--- lib/fifo.rb
+++ lib/fifo.rb
@@ -1,6 +1,6 @@
-require 'fifo/payload'
-require 'fifo/queue'
-require 'fifo/queue_manager'
+require File.dirname(__FILE__) + '/fifo/payload'
+require File.dirname(__FILE__) + '/fifo/queue'
+require File.dirname(__FILE__) + '/fifo/queue_manager'
 
 module FIFO
 end
